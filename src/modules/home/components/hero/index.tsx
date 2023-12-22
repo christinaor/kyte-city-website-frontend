bfa81ca278cfd6e85c50923c18fbaffe5f08b56a
Christina Or <christinaor.tech@gmail.com>
2023-12-22 00:12:54 -0500

    Fix subtext for xsmall screen

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -8,8 +8,7 @@ const Hero = () => {
   const router = useRouter();
 
   return (
-    //   <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-    <div className="h-[60vh] w-full relative bg-ui-bg-subtle xsmall:40vh">
+    <div className="h-[60vh] w-full relative bg-ui-bg-subtle xsmall:h-[40vh]">
       <div className="absolute inset-0">
         <Image
           src="/images/hero.jpg"
@@ -19,7 +18,7 @@ const Hero = () => {
         />
         <div className="inline-flex flex-col shrink-0 justify-center gap-4 pl-4 w-10/12 h-full text-secondary-1 xsmall:pl-16">
           <h2 className="h2-mobile-regular drop-shadow-md text-secondary-1 xsmall:h2-desktop-regular">Be a Part of the Future of Delivery in Los Angeles</h2>
-          <p className="p1-mobile-regular max-w-2/3 drop-shadow-md text-main-2 xsmall:p1-desktop-regular">Join Kyte today and experience a whole new level of convenience, speed, and efficiency.</p>
+          <p className="p1-mobile-regular max-w-2/3 drop-shadow-md text-main-2 xsmall:p1-desktop-regular xsmall:w-2/3">Join Kyte today and experience a whole new level of convenience, speed, and efficiency.</p>
           <Button 
             className="flex flex-col shrink-0 justify-center items-center text-center px-5 py-1 w-min h-8 rounded-sm micro-mobile-bold text-main-1 bg-secondary-2 z-40 whitespace-nowrap" 
             onClick={() => router.push("/shop-by/bestsellers")}
